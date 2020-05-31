b6cfac1ee3a2b6e3263e73b266be6fb86081b3c4
pipitnap <66130238+pipitnap@users.noreply.github.com>
2020-05-31 23:06:37 +0700

    Add files via upload

---
+++ tiket.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tiket Kaget</title>
+	
+	<link rel="stylesheet" type="text/css" href="animate.css">
+	<link rel="stylesheet" type="text/css" href="animate.css">	
+	<link rel="stylesheet" type="text/css" href="AdminLTE.css">
+	<link rel="stylesheet" type="text/css" href="AdminLTE.min.css">	
+ 	<link href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@700&family=Indie+Flower&display=swap" rel="stylesheet">
+ 	<link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
+ 	<link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
+	<script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+ 	<style type="text/css">
+ 		#judul
+ 		{
+ 			font-family: 'Bangers', cursive;
+ 		}
+
+ 	</style>
+ </head>
+<body style="background-color: #F0FFFF">	
+	<center><h1 id="judul" class="animated fadeInUp"> UPS!</h1>
+		<img src="img/Tiket.jpg" class="animated bounceIn" class="img-responsive" style="width: 80%"></a>		
+	</center>
+</body>
+</html>
